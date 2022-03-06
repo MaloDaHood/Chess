408c1bfd07176c6050a5a2c34f9b59ce9020a303
MaloDaHood <malomr2002@gmail.com>
2022-03-06 23:24:37 +0100

    Started working on spawning pieces in

--- src/main.py
+++ src/main.py
@@ -7,9 +7,38 @@ if __name__ == "__main__":
     pygame.init()
     game = Game()
     board = Board()
-    board.display_board(game.get_window())
     
-    queenB = Queen("B", (300, 0), True)
-    board.display_piece(game.get_window(), queenB)
+    pawns = rooks = []
+    x = 0
+    for i in range(2):
+        color = "B" if i == 0 else "W"
+        
+        # Creation of all the pawns
+        for j in range(8):
+            y = 100 if i == 0 else 600
+            pawns.append(Pawn(color, (x + (j * 100), y)))
+            
+        # Creation of all the rooks
+        for j in range(2):
+            y = 0 if i == 0 else 700
+            rooks.append(Rook(color, (x + (j * 700), y)))
+            
+        x = 0
     
-    game.run()
+    # Main game loop
+    running = True
+    while(running):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                
+        board.display_board(game.get_window())
+        board.display_pieces(game.get_window(), pawns)
+        board.display_pieces(game.get_window(), rooks)
+        
+        game.update()
+        
+        if game.is_over():
+            running = False
+    
+    pygame.quit()
