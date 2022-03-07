82ae0d9642e2639126ee93d4c4bcd00121acfeb0
MaloDaHood <malomr2002@gmail.com>
2022-03-07 18:35:30 +0100

    Removed color parameter in Piece class

--- src/game.py
+++ src/game.py
@@ -17,63 +17,37 @@ class Game:
     
     def update(self) -> None:
         pygame.display.flip()
-        
-    # def spawn_pieces_loop(self) -> list[list[Piece]]:
-        
-    #     pawns = rooks = knights = bishops = queens = kings = []
-                
-    #     for i in range(2):
-    #         color = "B" if i == 0 else "W"
-            
-    #         # Creation of all the pawns
-    #         for j in range(8):
-    #             y = 100 if i == 0 else 600
-    #             pawns.append(Pawn(color, (j * 100, y)))
-                
-    #         y = 0 if i == 0 else 700
-    #         # Creation of all the rooks, knights and bishops
-    #         for j in range(2):
-    #             rooks.append(Rook(color, (j * 700, y)))
-    #             knights.append(Knight(color, (100 + (j * 500), y)))
-    #             bishops.append(Bishop(color, (200 + (j * 300), y)))
-            
-    #         # Creation of the queens and kings
-    #         queens.append(Queen(color, (300, y)))
-    #         kings.append(King(color, (400, y)))
-            
-    #     return [pawns, rooks, knights, bishops, queens, kings]
     
     def spawn_pieces(self, board :list[list[str]]) -> list[Piece]:
             
         pieces = []
         
-        for i in range(8):
+        for i in range(len(board)):
 
-            for j in range(8):
+            for j in range(len(board[i])):
                 
-                color = board[i][j][1]
-                coord = ((j * 100), (i *100))
+                coord = ((j * 100), (i * 100))
                 id = board[i][j]
                 
-                if board[i][j][0] == "":
+                if id[0] == " ":
                     pass
                 
-                elif board[i][j][0] == "P":
-                    pieces.append(Pawn(color, coord, id))
+                elif id[0] == "P":
+                    pieces.append(Pawn(coord, id))
                     
-                elif board[i][j][0] == "R":
-                    pieces.append(Rook(color, coord, id))
+                elif id[0] == "R":
+                    pieces.append(Rook(coord, id))
                     
-                elif board[i][j][0] == "H":
-                    pieces.append(Knight(color, coord, id))
+                elif id[0] == "H":
+                    pieces.append(Knight(coord, id))
                     
-                elif board[i][j][0] == "B":
-                    pieces.append(Bishop(color, coord, id))
+                elif id[0] == "B":
+                    pieces.append(Bishop(coord, id))
                     
-                elif board[i][j][0] == "Q":
-                    pieces.append(Queen(color, coord, id))
+                elif id[0] == "Q":
+                    pieces.append(Queen(coord, id))
                     
-                elif board[i][j][0] == "K":
-                    pieces.append(King(color, coord, id))
+                elif id[0] == "K":
+                    pieces.append(King(coord, id))
                     
         return pieces
--- src/piece.py
+++ src/piece.py
@@ -3,19 +3,30 @@ import sys
 sys.path.append('../')
 
 class Piece:
-    def __init__(self, color :str, position :tuple, id :str) -> None:
-        self.color = color # Either "B" or "W"
+    def __init__(self, position :tuple[int , int], id :str) -> None:
         self.position = position
         self.id = id # Ex: RB1 -> Rook Black 1
+        self.color = id[1] # Either "B" or "W"
         self.isAlive = True
         self.hasMoved = False
-        self.image = pygame.image.load("assets/blank.png")
+        self.image = pygame.image.load("assets/" + id[:-1] + ".png")
         
-    def get_position(self) -> tuple:
+    def get_position(self) -> tuple[int, int]:
         return self.position
+    
+    def get_id(self) -> str:
+        return self.id
     
     def get_image(self) -> pygame.surface.Surface:
         return self.image
+    
+    def is_alive(self) -> bool:
+        return self.isAlive
+    
+    def die(self) -> None:
+        self.isAlive = False
+    
+    
     
     #! NEEDS TO BE A METHOD INSIDE SUB-CLASSES
     # Moves the piece to a different position
@@ -25,41 +36,32 @@ class Piece:
         self.hasMoved = True;
         return True
     
-    def die(self) -> None:
-        self.isAlive = False
-        
-    def is_alive(self) -> bool:
-        return self.isAlive
-    
-    def get_id(self) -> str:
-        return self.id
-    
 class King(Piece):
-    def __init__(self, color :str, position :tuple, id :str) -> None:
-        super().__init__(color, position, id)
-        self.image = pygame.image.load("assets/king" + self.color + ".png")
+    def __init__(self, position :tuple[int , int], id :str) -> None:
+        super().__init__(position, id)
+        pass
         
 class Queen(Piece):
-    def __init__(self, color :str, position :tuple, id :str) -> None:
-        super().__init__(color, position, id)
-        self.image = pygame.image.load("assets/queen" + self.color + ".png")
+    def __init__(self, position :tuple[int , int], id :str) -> None:
+        super().__init__(position, id)
+        pass
     
 class Rook(Piece):
-    def __init__(self, color :str, position :tuple, id :str) -> None:
-        super().__init__(color, position, id)
-        self.image = pygame.image.load("assets/rook" + self.color + ".png")
+    def __init__(self, position :tuple[int , int], id :str) -> None:
+        super().__init__(position, id)
+        pass
         
 class Bishop(Piece):
-    def __init__(self, color :str, position :tuple, id :str) -> None:
-        super().__init__(color, position, id)
-        self.image = pygame.image.load("assets/bishop" + self.color + ".png")
-        
+    def __init__(self, position :tuple[int , int], id :str) -> None:
+        super().__init__(position, id)
+        pass        
+    
 class Knight(Piece):
-    def __init__(self, color :str, position :tuple, id :str) -> None:
-        super().__init__(color, position, id)
-        self.image = pygame.image.load("assets/knight" + self.color + ".png")
-        
+    def __init__(self, position :tuple[int , int], id :str) -> None:
+        super().__init__(position, id)
+        pass
+            
 class Pawn(Piece):
-    def __init__(self, color :str, position :tuple, id :str) -> None:
-        super().__init__(color, position, id)
-        self.image = pygame.image.load("assets/pawn" + self.color + ".png")
+    def __init__(self, position :tuple[int , int], id :str) -> None:
+        super().__init__(position, id)
+        pass
