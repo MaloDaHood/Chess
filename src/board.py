7449a1ff992aa0a74d261a0d5b3514a175278674
MaloDaHood <malomr2002@gmail.com>
2022-03-07 18:04:54 +0100

    Put str board in constructor with ids on it

--- src/board.py
+++ src/board.py
@@ -7,17 +7,20 @@ class Board:
     
     def __init__(self) -> None:
         # Basic board with starting positions
-        self.board = [
-            [4, 2, 3, 5, 6, 3, 2, 4],
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [4, 2, 3, 5, 6, 3, 2, 4]
+          self.board = [
+            ["RB1", "HB1", "BB1", "QB1", "KB1", "BB2", "HB2", "RB2"],
+            ["PB1", "PB2", "PB3", "PB4", "PB5", "PB6", "PB7", "PB8"],
+            ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
+            ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
+            ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
+            ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
+            ["PW1", "PW2", "PW3", "PW4", "PW5", "PW6", "PW7", "PW8"],
+            ["RW1", "HW1", "BW1", "QW1", "KW1", "BW2", "HW2", "RW2"]
         ]
-        
+    
+    def get_board(self) -> list[list[str]]:
+        return self.board
+    
     # Displays the board on full window
     def display_board(self, window :pygame.surface.Surface) -> None:
         background = pygame.image.load("assets/board.png")
