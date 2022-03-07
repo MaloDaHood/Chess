21739c40521265a3f95c0feed6eaba3406210a70
MrKoro4 <youenn.denizet@gmail.com>
2022-03-07 21:04:20 +0100

    repair piece.py

--- src/piece.py
+++ src/piece.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('../')
 
 class Piece:
-    def __init__(self, position :tuple[int , int], id :str) -> None:
+    def __init__(self, position :"tuple[int , int]", id :str) -> None:
         self.position = position
         self.id = id # Ex: "RB1" -> Rook Black 1
         self.color = id[1] # Either "B" or "W"
@@ -11,7 +11,7 @@ class Piece:
         self.hasMoved = False
         self.image = pygame.image.load("assets/" + id[:-1] + ".png")
         
-    def get_position(self) -> tuple[int, int]:
+    def get_position(self) -> "tuple[int, int]":
         return self.position
     
     def get_id(self) -> str:
@@ -30,38 +30,38 @@ class Piece:
     
     #! NEEDS TO BE A METHOD INSIDE SUB-CLASSES
     # Moves the piece to a different position
-    def move_to(self, position :tuple[int, int]) -> bool:
+    def move_to(self, position :"tuple[int, int]") -> bool:
         # If move is legal
         self.position = position
         self.hasMoved = True;
         return True
     
 class King(Piece):
-    def __init__(self, position :tuple[int , int], id :str) -> None:
+    def __init__(self, position :"tuple[int , int]", id :str) -> None:
         super().__init__(position, id)
         pass
         
 class Queen(Piece):
-    def __init__(self, position :tuple[int , int], id :str) -> None:
+    def __init__(self, position :"tuple[int , int]", id :str) -> None:
         super().__init__(position, id)
         pass
     
 class Rook(Piece):
-    def __init__(self, position :tuple[int , int], id :str) -> None:
+    def __init__(self, position :"tuple[int , int]", id :str) -> None:
         super().__init__(position, id)
         pass
         
 class Bishop(Piece):
-    def __init__(self, position :tuple[int , int], id :str) -> None:
+    def __init__(self, position :"tuple[int , int]", id :str) -> None:
         super().__init__(position, id)
         pass        
     
 class Knight(Piece):
-    def __init__(self, position :tuple[int , int], id :str) -> None:
+    def __init__(self, position :"tuple[int , int]", id :str) -> None:
         super().__init__(position, id)
         pass
             
 class Pawn(Piece):
-    def __init__(self, position :tuple[int , int], id :str) -> None:
+    def __init__(self, position :"tuple[int , int]", id :str) -> None:
         super().__init__(position, id)
         pass
