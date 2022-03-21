76ceabae7718c167567578b68d78f0f7506f6cd9
MaloDaHood <malomr2002@gmail.com>
2022-03-21 23:02:04 +0100

    Made the mouse invisible while dragging a piece

--- test/main_test.py
+++ test/main_test.py
@@ -17,11 +17,15 @@ if __name__ == "__main__":
     while(game.is_running()):
         
         game.handle_inputs(board, pieces)
-                    
+        
+        # We display the board in the background           
         board.display_board(window)
         
         # We Check if the player is currently dragging a piece
         if game.is_dragging():
+            
+            pygame.mouse.set_visible(False)
+            
             # We check if the player has selected an empty spot as the origin
             if board.get_id(game.get_drag_origin()) == "   ":
                 # Stop the drag
@@ -30,8 +34,13 @@ if __name__ == "__main__":
             else:
                 pieces[board.get_id(game.get_drag_origin())].center_on_pointer()
 
+            # We display the blue and green squares
             board.display_markers(window, pieces[game.get_drag_id()].get_legal_moves(board), game.get_drag_origin())
             
+        else:
+            pygame.mouse.set_visible(True)
+            
+        # We display all the pieces on top of the board
         board.display_pieces(window, pieces)
 
         # We update the window each frame
